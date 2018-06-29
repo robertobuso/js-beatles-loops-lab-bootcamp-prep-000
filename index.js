f2987bd6ba663bd11ae12e752d1419178a60b410
Roberto Busó-García <robertobuso@gmail.com>
2018-06-29 19:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function johnLennonFacts (facts) {
   return facts;
 }
 
+function iLoveTheBeatles (number) {
+  let newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+      } while (number < 15); {
+        newArray.push("I love the Beatles!");
+        number++;
+      }
+         return newArray;
+ 
+}
