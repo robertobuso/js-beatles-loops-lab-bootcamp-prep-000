8401920679ac91ff347b6c75091e40b380d42bd5
Roberto Busó-García <robertobuso@gmail.com>
2018-06-29 19:42:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function iLoveTheBeatles (number) {
   let newArray = [];
   do {
     newArray.push("I love the Beatles!");
-      } while (number > 15); {
+      } while (number < 15); {
         newArray.push("I love the Beatles!");
-        number = number + 1;
+        number++;
       }
          return newArray;
  
